77169b6360257fa05bf7f4a4ae752cc09d3b6133
Sionparadox <sion9999@naver.com>
2024-11-26 14:45:19 +0900

    feat: mongoDB 초기 세팅

--- src/main/java/com/example/onehada/db/mongodb/UserRepositori.java
+++ src/main/java/com/example/onehada/db/mongodb/UserRepositori.java
@@ -2,6 +2,5 @@ package com.example.onehada.db.mongodb;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-// MongoDB에서 User 데이터를 처리하는 리포지토리
 public interface UserRepositori extends MongoRepository<User, String> {
 }
