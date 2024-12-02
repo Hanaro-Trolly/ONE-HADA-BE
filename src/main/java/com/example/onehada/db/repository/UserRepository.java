f1f6a9eece09d04bb191d0ba0664b8b6504b98bd
mingwan <ibini12@gmail.com>
2024-12-02 19:27:29 +0900

    feat: admin user search

--- src/main/java/com/example/onehada/db/repository/UserRepository.java
+++ src/main/java/com/example/onehada/db/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.onehada.db.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,7 @@ import com.example.onehada.db.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUserEmail(String email);
+	List<User> findByUserNameContaining(String userName);
+	List<User> findByUserBirth(String userBirth);
+	List<User> findByUserNameContainingAndUserBirth(String userName, String userBirth);
 }
