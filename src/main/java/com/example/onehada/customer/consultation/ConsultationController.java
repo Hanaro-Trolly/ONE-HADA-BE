1b6f92ebaccc999eb52fe514d9865b2b7177a410
hyemch <blossoming33@gmail.com>
2024-12-17 15:57:23 +0900

    fix: json 보내는 형식 수정

--- src/main/java/com/example/onehada/customer/consultation/ConsultationController.java
+++ src/main/java/com/example/onehada/customer/consultation/ConsultationController.java
@@ -2,6 +2,8 @@ package com.example.onehada.customer.consultation;
 
 import java.util.List;
 
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -23,6 +25,6 @@ public class ConsultationController {
 	@GetMapping("")
 	public ResponseEntity<ApiResponse> getConsultations(@RequestHeader("Authorization") String token) {
 		List<ConsultationDTO> consultations = consultationService.getConsultations(token);
-		return ResponseEntity.ok(new ApiResponse(200, "OK", "상담내역 조회 성공", consultations));
+		return ResponseEntity.ok(new ApiResponse(200, "OK", "상담내역 조회 성공", Map.of("consultations", consultations)));
 	}
 }
