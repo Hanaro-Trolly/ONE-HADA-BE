63d26c14d847e611f3863fa3a5c718ba378d4c8f
somea82 <joemoon0526@gmail.com>
2024-12-19 09:58:52 +0900

    fix : 거래내역 조회 날짜, view 수정

--- src/main/java/com/example/onehada/customer/transaction/TransactionRepository.java
+++ src/main/java/com/example/onehada/customer/transaction/TransactionRepository.java
@@ -13,8 +13,8 @@ import com.example.onehada.customer.account.Account;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 	@Query("SELECT t FROM Transaction t WHERE " +
-		"(t.senderAccount = :account AND LOWER(t.senderName) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
-		"OR (t.receiverAccount = :account AND LOWER(t.receiverName) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
+		"((t.senderAccount = :account AND LOWER(t.receiverName) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
+		"OR (t.receiverAccount = :account AND LOWER(t.senderName) LIKE LOWER(CONCAT('%', :keyword, '%')))) " +
 		"AND t.transactionDate BETWEEN :startDate AND :endDate")
 	List<Transaction> findByAccountDateRangeAndKeyword(
 		@Param("account") Account account,
