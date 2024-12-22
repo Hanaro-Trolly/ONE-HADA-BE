b6028f7a32e74d28616bc9101306e39f27361481
hyemch <blossoming33@gmail.com>
2024-12-22 20:05:42 +0900

    feat: HistoryControllerTest 작성완료

--- src/main/java/com/example/onehada/customer/history/HistoryDTO.java
+++ src/main/java/com/example/onehada/customer/history/HistoryDTO.java
@@ -5,15 +5,19 @@ import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class HistoryDTO {
 	private Long historyId;
